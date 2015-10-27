ec21d5db77c3de8559cdd8e348ba1b4f22322e84
Sami Shumiye <sami.shumiye@telesishq.com>
2015-10-27 11:20:05 -0400

    second commit

--- Github/src/MyPackage/MyClass.java
+++ Github/src/MyPackage/MyClass.java
@@ -1,5 +1,14 @@
 package MyPackage;
 
+import org.testng.annotations.Test;
+
 public class MyClass {
+	
+	@Test
+	public void log(){
+		System.out.println("testing");
+		System.out.println("hello");
+		System.out.println("testing");
+	}
 
 }
