590df04ef5e3318de07a5443b7ff577a86f4b2cf
Sami Shumiye <sami.shumiye@telesishq.com>
2015-10-28 14:56:58 -0400

    adding comment

---
+++ Report/src/VC/CV.java
@@ -0,0 +1,34 @@
+package VC;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class CV {
+	
+	@Test 
+	public void th() throws InterruptedException{
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http:\\jqueryui.com");
+		driver.manage().window().maximize();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("javascript:window.scrollBy(0,350)");
+        Thread.sleep(3000);
+        driver.quit();
+        
+       //alternate way
+       /** WebElement element = driver.findElement(By.xpath(".//*[@id='sidebar']/aside[4]/ul/li[1]/a"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element);
+        Thread.sleep(4000);
+        actions.click();
+        actions.perform();
+        **/
+           
+		
+	}
+}
